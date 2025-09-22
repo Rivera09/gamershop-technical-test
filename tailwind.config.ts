e0c4d54b4768aea853808992f3c8d51ee3e5cc67
Jorge Rivera <jorge-421@hotmail.com>
2025-09-22 15:49:30 -0600

    refactor: update tailwind configuration file
    new colors added on tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,9 +15,12 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
+        white: "#fff",
         primary: "#3B3B3B",
+        "stone-100": "#F5F5F4",
         "fill-primary": "#585660",
         "stroke-secondary": "#8F8F8F",
+        "stroke-tertiary": "#EFEDF3",
         "neutral-500": "#737373",
         "neutral-700": "#404040",
         "surface-secondary": "#EEEEEE",
@@ -73,6 +76,22 @@ const config: Config = {
       "xl/xl-bold": [
         "24px",
         { fontWeight: 700, lineHeight: "28px", letterSpacing: "0.4px" },
+      ],
+      "button-mobile": [
+        "14px",
+        {
+          fontWeight: 700,
+          lineHeight: "16px",
+          letterSpacing: "0.5px",
+        },
+      ],
+      "button-desktop": [
+        "16px",
+        {
+          fontWeight: 700,
+          lineHeight: "16px",
+          letterSpacing: "0.5px",
+        },
       ],
     },
   },

e809143253c71eeccfbdc92b7c87268b3fa222b4
Jorge Rivera <jorge-421@hotmail.com>
2025-09-22 15:51:34 -0600

    feat: update footer and navbar
    Update footer and navbar layout components

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import logo from "@/assets/icons/logo.svg";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div className="bg-[#404040] py-[60px] flex justify-center">Footer</div>
-  )
-}
+    <div className="bg-neutral-700 py-[60px] flex justify-center">
+      <Link href="/">
+        <Image priority src={logo} alt="Apply digital" className="w-[170px]" />
+      </Link>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/layout/Navbar/index.tsx
+++ src/layout/Navbar/index.tsx
@@ -1,12 +1,18 @@
-import Link from "next/link"
+import Link from "next/link";
+import CartIcon from "@/assets/icons/cart.svg";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="px-6 py-5 bg-[#EEEEEE] flex justify-between">
-      <Link href="/">GamerShop</Link>
-      <Link href="/cart">C</Link>
+    <div className="px-6 py-5 bg-surface-secondary ">
+      <div className="container flex justify-between">
+        <Link href="/">GamerShop</Link>
+        <Link href="/cart">
+          <Image priority src={CartIcon} alt="cart" className="size-6" />
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
